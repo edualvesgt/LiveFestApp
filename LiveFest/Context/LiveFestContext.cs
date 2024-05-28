223e5c2390f31943f8f5aae54380c3aefc2239a9
Eduardo Alves Araújo Silva <46291995867@info.local>
2024-05-28 10:55:26 -0300

    Migration realiazada

---
+++ LiveFest/Context/LiveFestContext.cs
@@ -0,0 +1,26 @@
+﻿using LiveFest.Domains;
+using Microsoft.EntityFrameworkCore;
+
+namespace LiveFest.Context
+{
+    public class LiveFestContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Address> Address { get; set; }
+        public DbSet<Categories> Categories { get; set; }
+        public DbSet<Events> Events { get; set; }
+        public DbSet<Evaluation> Evaluation { get; set; }
+        public DbSet<SaveEvents> SaveEvents { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="optionsBuilder"></param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server= SUPORTE\\SQLEXPRESS; Database= LiveFestServer; User Id=sa; Pwd=Senai@134; TrustServerCertificate= True");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
