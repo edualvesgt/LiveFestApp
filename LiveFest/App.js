23f2c44defc771ca0c0a6794db550aabebccfb73
Lucas Ribeiro De Lacerda <32784630803@info.local>
2024-05-28 10:40:54 -0300

    fonts

--- LiveFest/App.js
+++ LiveFest/App.js
@@ -1,10 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { SplashScreen } from './src/screens/Splash/Splash';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { SplashScreen } from "./src/screens/Splash/Splash";
 
 // Importe das Fontes
 import {
@@ -12,23 +11,25 @@ import {
   MontserratAlternates_600SemiBold,
   MontserratAlternates_500Medium,
   MontserratAlternates_700Bold,
-} from '@expo-google-fonts/montserrat-alternates';
+} from "@expo-google-fonts/montserrat-alternates";
+
 import {
-  Quicksand_500Medium,
-  Quicksand_600SemiBold
-} from '@expo-google-fonts/quicksand';
+  useFonts,
+  Raleway_500Medium,
+  Raleway_600SemiBold,
+  Raleway_700Bold,
+} from "@expo-google-fonts/raleway";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-
-
   const [fontsLoaded, fontError] = useFonts({
     MontserratAlternates_600SemiBold,
     MontserratAlternates_500Medium,
     MontserratAlternates_700Bold,
-    Quicksand_500Medium,
-    Quicksand_600SemiBold,
+    Raleway_500Medium,
+    Raleway_600SemiBold,
+    Raleway_700Bold,
   });
 
   if (!fontsLoaded && !fontError) {
@@ -37,25 +38,22 @@ export default function App() {
 
   return (
     <NavigationContainer>
-    <Stack.Navigator initialRouteName="SplashScreen">
-
-      <Stack.Screen
-        name="SplashScreen"
-        component={SplashScreen}
-        options={{ headerShown: false }}
-      />
-
-      
-    </Stack.Navigator>
-  </NavigationContainer>
+      <Stack.Navigator initialRouteName="SplashScreen">
+        <Stack.Screen
+          name="SplashScreen"
+          component={SplashScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
