3047e704b96c9b655172de3977e15927ac55d791
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-06-03 07:14:01 -0300

    Criado Input

--- LiveFest/src/components/InputSearch/InputSearch.js
+++ LiveFest/src/components/InputSearch/InputSearch.js
@@ -1,10 +1,69 @@
+import { StyleSheet, Text, View } from "react-native";
 import { InputSearchStyle } from "./Style";
+import { useState } from "react";
+
+dataSelect = [
+  { label: 'Item 1', value: '1' },
+  { label: 'Item 2', value: '2' },
+  { label: 'Item 3', value: '3' },
+  { label: 'Item 4', value: '4' },
+  { label: 'Item 5', value: '5' },
+  { label: 'Item 6', value: '6' },
+  { label: 'Item 7', value: '7' },
+  { label: 'Item 8', value: '8' },
+]
 
 
-export default function InputSearch (){
-  return(
-    <>
-    <InputSearchStyle></InputSearchStyle>
-    </>
+export default function InputSearch() {
+  const [value, setValue] = useState(null);
+  const [isFocus, setIsFocus] = useState(false);
+
+  // const renderLabel = ({value, isFocus}) => {
+  //   if (value || isFocus) {
+  //     return (
+  //       <Text style={[styles.label, isFocus && { color: '#956ADF' }]}>
+  //         Eventos e locais
+  //       </Text>
+  //     );
+  //   }
+  //   return null;
+  // };
+
+  return (
+    <View style={styles.container}>
+      {/* {renderLabel({value,isFocus})} */}
+      <InputSearchStyle
+        isFocus={isFocus}
+        value={value}
+        setIsFocus={setIsFocus}
+        setValue={setValue}
+        data={dataSelect}
+        style={[styles.dropdown, isFocus && { borderColor: '#956ADF' }]}
+      />
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    padding: 16,
+  },
+  dropdown: {
+    height: 50,
+    borderColor: '#956ADF',
+    borderWidth: 2,
+    borderRadius: 100,
+    paddingHorizontal: 8,
+
+  },
+  label: {
+    position: 'absolute',
+    backgroundColor: 'white',
+    left: 22,
+    top: 8,
+    zIndex: 999,
+    paddingHorizontal: 8,
+    fontSize: 14,
+  }
+});
--- LiveFest/src/components/InputSearch/Style.js
+++ LiveFest/src/components/InputSearch/Style.js
@@ -1,36 +1,65 @@
 import styled from "styled-components";
-import {Dropdown} from 'react-native-element-dropdown';
-import { useState } from "react";
-import { StyleSheet } from "react-native";
-import AntDesign from '@expo/vector-icons/AntDesign';
+import { Dropdown } from 'react-native-element-dropdown';
+import { Image, StyleSheet, TouchableOpacity, View } from "react-native";
+import { MaterialIcons } from '@expo/vector-icons';
 
+export const InputSearchStyle = styled(Dropdown).attrs(props => ({
+  mode: "auto",
+  placeholderStyle: styles.placeholderStyle,
+  selectedTextStyle: styles.selectedTextStyle,
+  inputSearchStyle: styles.inputSearchStyle,
+  iconStyle: styles.iconStyle,
+  data: props.data,
+  search: true,
+  maxHeight: 300,
+  labelField: "label",
+  valueField: "value",
+  placeholder: !props.isFocus ? 'Eventos e locais' : '...',
+  searchPlaceholder: "Pesquisar...",
+  onFocus: () => { props.setIsFocus(true) },
+  onBlur: () => { props.setIsFocus(false) },
+  onChange: item => {
+    props.setValue(item.value); // Supondo que setValue seja uma função prop
+    props.setIsFocus(false); // Supondo que setIsFocus seja uma função prop
+  },
+  renderLeftIcon: () => (
+    <Image
+      source={require("../../../assets/TicketLiveFest.png")}
+      style={{ height: '80%', width: 50, resizeMode: "contain" }}
+    />
+  ),
+  renderRightIcon: () => {
+    if (props.value) {
+      return (
+        <View style={{ width: 50, height: "100%", justifyContent: "center", alignItems: "center" }}>
+          <TouchableOpacity
+            onPress={() => {
+              props.setValue(null)
+            }}
+            style={{ width: "100%", height: "100%", justifyContent: "center", alignItems: "center" }}
+          >
+            <MaterialIcons name="clear" size={24} color="black" />
+          </TouchableOpacity>
+        </View>
+      )
+    } else {
+      return (
+        <Image
+          source={require("../../../assets/magnifyingGlass.png")}
+          style={{ height: '60%', width: 50, resizeMode: "contain" }}
+        />
+      )
+    }
+  }
+}))`
+
+`
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-    padding: 16,
-  },
-  dropdown: {
-    height: 50,
-    borderColor: 'gray',
-    borderWidth: 0.5,
-    borderRadius: 8,
-    paddingHorizontal: 8,
-  },
-  icon: {
-    marginRight: 5,
-  },
-  label: {
-    position: 'absolute',
-    backgroundColor: 'white',
-    left: 22,
-    top: 8,
-    zIndex: 999,
-    paddingHorizontal: 8,
-    fontSize: 14,
-  },
   placeholderStyle: {
-    fontSize: 60,
+    fontSize: 16,
+    color: "#000",
+    fontFamily: "MontserratAlternates_500Medium"
   },
   selectedTextStyle: {
     fontSize: 16,
@@ -44,36 +73,3 @@ const styles = StyleSheet.create({
     fontSize: 20,
   },
 });
-
-
-export const InputSearchStyle = styled(Dropdown).attrs(props => ({
-  placeholderStyle:styles.placeholderStyle,
-  selectedTextStyle:styles.selectedTextStyle,
-  inputSearchStyle:styles.inputSearchStyle,
-  iconStyle:styles.iconStyle,
-  data:[["dsadas"],["dsadsadas"]],
-  search: true,
-  maxHeight:300,
-  labelField:"label",
-  valueField:"value",
-  placeholder:!props.isFocus ? 'Selecione um item' : '...',
-  searchPlaceholder:"Pesquisar...",
-  value:props.value,
-  onFocus:props.onFocus,
-  onBlur:props.onBlur,
-  onChange:item => {
-    props.setValue(item.value); // Supondo que setValue seja uma função prop
-    props.setIsFocus(false); // Supondo que setIsFocus seja uma função prop
-  },
-  renderLeftIcon:() => (
-    <AntDesign
-      style={styles.icon}
-      color={props.isFocus ? 'blue' : 'black'}
-      name="Safety"
-      size={20}
-    />
-  ),
-}))`
-
-`
-
